57d88e6e2f39d94580bdbc923a0dc8b49c8a0d8d
anilsaini <asaini@hgsdigital.com>
2021-11-04 07:51:40 +0530

    Project setup with material ui

---
+++ movie_app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Header/Header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <span>Entertainment Hub</span>
+        </div>
+    )
+}
+
+export default Header
